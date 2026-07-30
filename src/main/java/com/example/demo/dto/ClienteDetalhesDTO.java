056f6c181dabcbc24977f7998773cddc8326f9b6
william-grassis67 <roquew890@gmail.com>
2026-07-30 09:49:26 -0300

    melhorias adiocionada

--- src/main/java/com/example/demo/dto/ClienteDetalhesDTO.java
+++ src/main/java/com/example/demo/dto/ClienteDetalhesDTO.java
@@ -1,13 +1,16 @@
 package com.example.demo.dto;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ClienteDetalhesDTO {
 
     private UsuarioDTO usuario;
-    private List<ProcessoDTO> processos;
-    private List<GuiaInssDTO> guias;
+    private List<ProcessoDTO> processos = new ArrayList<>();
+    private List<GuiaInssDTO> guias = new ArrayList<>();
 
+    public ClienteDetalhesDTO() {
+    }
 
     public ClienteDetalhesDTO(
             UsuarioDTO usuario,
@@ -19,18 +22,27 @@ public class ClienteDetalhesDTO {
         this.guias = guias;
     }
 
-
     public UsuarioDTO getUsuario() {
         return usuario;
     }
 
+    public void setUsuario(UsuarioDTO usuario) {
+        this.usuario = usuario;
+    }
 
     public List<ProcessoDTO> getProcessos() {
         return processos;
     }
 
+    public void setProcessos(List<ProcessoDTO> processos) {
+        this.processos = processos;
+    }
 
     public List<GuiaInssDTO> getGuias() {
         return guias;
+    }
+
+    public void setGuias(List<GuiaInssDTO> guias) {
+        this.guias = guias;
     }
 }
