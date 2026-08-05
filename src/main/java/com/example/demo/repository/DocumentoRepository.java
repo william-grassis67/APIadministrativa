0ea9b051fba94aa08336826540d1513d6c79828f
william <roquew890@gmail.com>
2026-08-05 20:26:24 -0300

    chore: backend pronto

--- src/main/java/com/example/demo/repository/DocumentoRepository.java
+++ src/main/java/com/example/demo/repository/DocumentoRepository.java
@@ -1,9 +1,13 @@
 package com.example.demo.repository;
 
+import com.example.demo.entity.Documento;
+import com.example.demo.entity.Processo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.example.demo.entity.Documento;
+import java.util.List;
 
-public interface DocumentoRepository extends JpaRepository<Documento, Long>{
+public interface DocumentoRepository extends JpaRepository<Documento, Long> {
+
+List<Documento> findByProcesso(Processo processo);
 
 }
